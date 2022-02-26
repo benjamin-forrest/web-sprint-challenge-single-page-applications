155cd6115369e71a11c196ade32e46e401ab9c93
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-02-25 23:50:17 -0800

    css and fixed bloomtech branding

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
     return (
         <div className='nav-Style'>
             <nav className='nav-bar'>
-            <h1>LAMBDA EATS</h1>
+            <h1>Bloom Eats</h1>
             <div className='nav-links'>
                 <Link to='/'>Home</Link>
                 <Link to='/Pizza'>Order</Link>
