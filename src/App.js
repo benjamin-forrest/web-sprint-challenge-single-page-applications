5dfc9f55697de32265e69cb03daf2a568e5956a7
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2022-02-25 23:40:07 -0800

    appjs imports and initial form values

--- src/App.js
+++ src/App.js
@@ -1,4 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar";
+import { Route, Switch } from 'react-router-dom'
+import Home from "./Home";
+import PizzaForm from "./PizzaForm";
+import './App.css'
+import axios from "axios";
+import schema from './validation/schema'
+import * as yup from 'yup'
+import './index.css'
+
+const initialFormValues = {
+  //Text Input
+  name: '',
+  extras: '',
+  //Radio buttons
+  sauce: '',
+  //dropDown
+  size: '',
+  //CheckBoxes
+  cheese: false,
+  pepperonie: false,
+  sausage: false,
+  onion: false,
+  pineapple: false,
+  greenPepper: false,
+  corn: false,
+}
+
+const initialFormErrors = {
+  name: '',
+  extras: '',
+  sauce: '',
+  size: ''
+}
+
+const initialPizza = []
+const initialDisabled = false
+
+
+
 
 const App = () => {
   return (
